9e7f0b495f0ae9a47d0e978c8975ad045e18784a
Gerard McHale <gerardmchale1+github@gmail.com>
2023-01-17 17:29:14 +0000

    Updates for Black

--- py_dev_visual_studio_code/sieve.py
+++ py_dev_visual_studio_code/sieve.py
@@ -3,5 +3,5 @@ sieve = [True] * 101
 for i in range(2, 100):
     if sieve[i]:
         print(i)
-        for j in range(i*i, 100, i):
+        for j in range(i * i, 100, i):
             sieve[j] = False
